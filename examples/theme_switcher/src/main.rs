964bddfae7a019823306310b26f4f030409bdfbf
Spencer C. Imbleau <spencer@imbleau.com>
2022-10-16 14:19:09 -0400

    Code cleanup

--- examples/theme_switcher/src/main.rs
+++ examples/theme_switcher/src/main.rs
@@ -18,6 +18,12 @@ pub fn root() -> Html {
 #[styled_component(App)]
 fn app() -> Html {
     let theme = use_theme::<MyThemeChoice>();
+    let style = css! {
+        & > * {
+            color: ${theme.fg};
+            background-color: ${theme.bg};
+        }
+    };
 
     let onclick = {
         let theme = theme.clone();
@@ -31,14 +37,7 @@ fn app() -> Html {
         })
     };
 
-    let style = css! {
-        & > * {
-            color: ${theme.fg};
-            background-color: ${theme.bg};
-        }
-    };
-
-    let text = match theme.kind() {
+    let theme_text = match theme.kind() {
         MyThemeChoice::Light => "Light",
         MyThemeChoice::Dark => "Dark",
         MyThemeChoice::Blue => "Blue",
@@ -47,7 +46,7 @@ fn app() -> Html {
     html! {
         <div class={style}>
             <button { onclick }>{"Switch themes"}</button>
-            <p>{"Current theme: "}{ text }</p>
+            <p>{"Current theme: "}{ theme_text }</p>
         </div>
     }
 }
