7cc811a4b1d7a2ca1a3c39ea348e6f5d90d6f03e
Spencer C. Imbleau <spencer@imbleau.com>
2022-12-20 09:15:48 -0500

    Added features

--- crates/themer/src/lib.rs
+++ crates/themer/src/lib.rs
@@ -2,17 +2,22 @@
 pub use themer_core::*;
 
 // Re-export macros
+#[cfg(feature = "macros")]
 pub mod macros {
     pub use themer_macros::*;
 }
 
 // Export Yew integration
+#[cfg(feature = "yew_integration")]
 pub mod yew;
 
 // Provide prelude
 pub mod prelude {
     pub use crate::browser::BrowserPreference;
-    pub use crate::macros::{theme, theme_key};
     pub use crate::traits::{Theme, ThemeKey};
+
+    #[cfg(feature = "macros")]
+    pub use crate::macros::{theme, theme_key};
+    #[cfg(feature = "yew")]
     pub use crate::yew::{use_theme, ThemeProvider};
 }
