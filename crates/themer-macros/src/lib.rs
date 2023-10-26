0db81c5009a5fd495396f6a957994c5fcdd89862
Spencer C. Imbleau <spencer@imbleau.com>
2023-10-25 22:33:28 -0400

    build: removed nightly dependance

--- crates/themer-macros/src/lib.rs
+++ crates/themer-macros/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(proc_macro_quote)]
 #![deny(clippy::all)]
 #![deny(unsafe_code)]
 #![deny(non_snake_case)]
@@ -10,35 +9,39 @@
     deny(dead_code, unused_imports)
 )]
 
-use proc_macro::{quote, TokenStream};
-use quote::ToTokens;
-use syn::{parse_macro_input, DeriveInput};
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, DeriveInput, Item};
 
 #[proc_macro_attribute]
 pub fn theme_key(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as Item);
     quote! {
         #[derive(::core::fmt::Debug)]
         #[derive(::core::clone::Clone)]
         #[derive(::core::marker::Copy)]
         #[derive(::core::hash::Hash)]
         #[derive(::core::cmp::PartialEq)]
-        $item
+        #input
     }
     .into()
 }
 
 #[proc_macro_attribute]
 pub fn theme(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let clone = item.clone();
-    let DeriveInput { ident, .. } = parse_macro_input!(clone);
+    let DeriveInput { ident, .. } = {
+        let item = item.clone();
+        parse_macro_input!(item)
+    };
     let name = ident.to_token_stream();
+    let input = parse_macro_input!(item as Item);
     quote! {
-        impl themer::traits::Theme for $name {}
+        impl themer::traits::Theme for #name {}
 
         #[derive(::core::clone::Clone)]
         #[derive(::core::fmt::Debug)]
         #[derive(::core::cmp::PartialEq)]
-        $item
+        #input
     }
     .into()
 }
